3a5e7b7244fa58908ec408be48665f226f111c61
Peach Paulison <dlpeachpaulison@gmail.com>
2021-11-16 15:09:03 -0500

    Completed through 12 Models & Model Binding.  Exercises in edit-events branch.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -14,6 +14,23 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = EventData.getById(eventId);
+        model.addAttribute("event", eventToEdit);
+        String title = "Edit Event " + eventToEdit.getName() +
+                "(id=" + eventToEdit.getId() +")";
+        model.addAttribute("title", title);
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        Event eventToEdit = EventData.getById(eventId);
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        return "redirect:";
+    }
 
     @GetMapping
     public String displayAllEvents(Model model) {
